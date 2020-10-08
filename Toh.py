9309bc0506ed1024b22fed81a96d524421432d72
pozm <44528100+pozm@users.noreply.github.com>
2020-10-08 22:06:30 +0100

    comments and minor changes

--- Toh.py
+++ Toh.py
@@ -1,40 +1,51 @@
-starting = [[1,2],[3,4],[5,6]]
+# should be able to edit this and it should stil work
+starting = [[2,1],[4,3],[6,5]]
 
 class tohInstance():
     def __init__(self):
         self.state = starting
         self.won = 0
+    # gets the last number in arr, this is used to get the "top" ring
     def getlast(self,pos:int):
         posObj = self.state[pos]
         return posObj[len(posObj)-1]
+    # scuffed check, basically goes through all towers and checks if the length 
+    # is 6 (if they all are on it), which should work because it does not move unless 
+    # it makes sense in the games rules.
     def checkIfWin(self):
         outcome = 0
+        ammount = sum([len(i) for i in starting]) # should be sum of all array values
         for tower in self.state:
-            if len(tower) == 9:
+            if len(tower) == ammount:
                 self.won = 1
                 outcome = 1
                 break
         return outcome
+    # move top ring from a tower to another
     def move(self,pos:int,newPos:int):
         posObj = self.state[pos]
         newPosObj = self.state[newPos]
         if pos == newPos:
-            return 1
-        if self.getlast(newPos) < self.getlast(pos):
+            return 1 #ok, but do nothing
+        if self.getlast(newPos) < self.getlast(pos): 
+            #ok, remove from old tower and add to new
             newPosObj.append( self.getlast(pos) )
             posObj.remove( self.getlast(pos) )
             if self.checkIfWin() == 1:
-                return 2
-            return 1
+                return 2 
+            return 1 
         else:
+            # bad move, return 0
             return 0
-
+# check if input is valid
 def inputHandler(out):
     secA = out.split(" ")
     if len(secA) != 2:
         return 0
-    return 1
-
+    if secA[0].isdigit() and secA[1].isdigit():
+        return 1
+    return 0
+# statr the game
 game = tohInstance()
 def play():
     print("Current state : ", game.state)
@@ -43,6 +54,7 @@ def play():
     if (isValid != 1):
         print("Invalid input")
         return play()
+    # parse to int
     pos1 = int(inp.split(" ")[0])-1
     pos2 = int(inp.split(" ")[1])-1
     outcome = game.move(pos1,pos2)
